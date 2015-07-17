4900eedd04552408280a3ae4bc4a7ef1ad899820
Hana Fallisgaard <hfgaard@gmail.com>
2015-07-16 23:05:54 -0700

    Create game where you select an animal to fight the computer.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 
 gulp.task('build', function() {
-  return gulp.src('src/*.js')
+  return gulp.src('*.js')
              .pipe(babel())
              .pipe(gulp.dest('built'));
 });
